570a43a97380bc6609127f9c0cb0e6ad9f60193c
Ilija Petkovic <ilija.petkovic@novelic.com>
2019-05-25 11:58:22 +0200

    CHANGE: main_v1.c - Add performance counter

--- ps/main_v1.c
+++ ps/main_v1.c
@@ -19,17 +19,24 @@
 #include "stdlib.h"
 #include "string.h"
 #include "altera_hostfs.h"
-//#include "altera_avalon_performance_counter.h"
+#include "altera_avalon_performance_counter.h"
 #include "alt_types.h"
+#include "io.h"
 #include "math.h"
 #include "altera_avalon_sgdma_regs.h"
 #include "altera_avalon_sgdma_descriptor.h"
 #include "altera_avalon_sgdma.h"
 #include "sys/alt_cache.h"
+#include "functions.h"
+//#include "altera_avalon_performance_counter.h"
+//#include "altera_avalon_timer.h"
+//#include "sys/alt_timestamp.h"
+
 
 #define MAX_HISTOGRAM_VALUE (255)
 #define MAX_HISTOGRAM_VALUE_FLOAT (255.0f)
-#define NUMBER_OF_BUFFERS (1)
+#define MAX_NUM_DMA_TRANSFER (65535)
+//#define NUMBER_OF_BUFFERS (1)
 
 /* These will gate the data checking near the end of main */
 volatile alt_u16 tx_done = 0;
@@ -58,25 +65,31 @@ int main()
 	alt_u32 tmp = 0;
 	alt_u8* image;
 	alt_u32* histogram;
+	alt_u32** hist_value;
 	alt_u32 buffer_counter = 0;
 
 	alt_u32 tmp22;
+
+	alt_u8 NUMBER_OF_BUFFERS = 0;
+    alt_u8 ** input_buffers;
+    alt_u16 * buffer_lengths;
+
+    alt_u32 it = 0;
+    alt_u32 itr = 0;
 
 	void * temp_ptr;
 
 	alt_sgdma_dev* sgdma_m2s = alt_avalon_sgdma_open("/dev/sgdma_m2s");
 	alt_sgdma_dev* sgdma_s2m = alt_avalon_sgdma_open("/dev/sgdma_s2m");
 
-	//alt_sgdma_descriptor *m2s_desc,
 	alt_sgdma_descriptor *m2s_desc_copy;
-	//alt_sgdma_descriptor *s2m_desc,
 	alt_sgdma_descriptor *s2m_desc_copy;
 	alt_sgdma_descriptor *transmit_descriptors, *receive_descriptors;
+	alt_u32 tmp_start_time_hw, tmp_end_time_hw, tmp_start_time_sw, tmp_end_time_sw;
 
-	input_file = fopen("/mnt/host/files/low_contrast64.bin","r");
+	input_file = fopen("/mnt/host/files/dark512.bin","r");
 	output_file = fopen("/mnt/host/files/histogram.bin","w");
 
-	//printf("START!\n");
 
 	if(input_file == NULL)
 	{
@@ -108,51 +121,6 @@ int main()
 		return 1;
 	}
 
-	// ALOCATION OF TRANCIVE DESCRIPTORS --> M2S
-	temp_ptr = malloc((NUMBER_OF_BUFFERS + 2) * ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE);
-
-	if(temp_ptr == NULL)
-	{
-		printf("Failed to allocate memory for the transmit descriptors\n");
-	    return 1;
-	}
-
-	m2s_desc_copy = (alt_sgdma_descriptor*)temp_ptr;
-
-	while((((alt_u32)temp_ptr) % ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE) != 0)
-	{
-		temp_ptr++;
-	}
-
-	transmit_descriptors = (alt_sgdma_descriptor *)temp_ptr;
-
-	//m2s_desc = transmit_descriptors;
-
-	transmit_descriptors[NUMBER_OF_BUFFERS].control = 0;
-
-	// ALLOCATION OF RECEIVE DESCRIPTORS
-	temp_ptr = malloc((NUMBER_OF_BUFFERS + 2) * ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE);
-
-	if(temp_ptr == NULL)
-	{
-		printf("Failed to allocate memory for the transmit descriptors\n");
-	    return 1;
-	}
-
-	s2m_desc_copy = (alt_sgdma_descriptor*)temp_ptr;
-
-	while((((alt_u32)temp_ptr) % ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE) != 0)
-	{
-		temp_ptr++;
-	}
-
-	receive_descriptors = (alt_sgdma_descriptor *)temp_ptr;
-
-	//s2m_desc = receive_descriptors;
-
-	receive_descriptors[NUMBER_OF_BUFFERS].control = 0;
-
-
 	// READING DATA
 
 	while(iterator < 4){
@@ -176,20 +144,149 @@ int main()
 
 	}
 
-	//printf("image_width = %u\n",image_width);
-	//printf("image_height = %u\n",image_height);
-
 	image = (alt_u8*)malloc(image_width*image_height*sizeof(alt_u8));
-	//image_out = (alt_u16*)malloc(image_width*image_height*sizeof(alt_u16));MAX_HISTOGRAM_VALUE
+
+	// NUMBER_OF_BUFFERS
+	NUMBER_OF_BUFFERS += (image_width*image_height)/MAX_NUM_DMA_TRANSFER;
+	if(image_width*image_height % MAX_NUM_DMA_TRANSFER > 0)
+	{
+
+		NUMBER_OF_BUFFERS++;
+
+	}
+
+	printf("NUMBER_OF_BUFFERS = %u\n",NUMBER_OF_BUFFERS);
+
+	buffer_lengths = (alt_u16 *)malloc(NUMBER_OF_BUFFERS*sizeof(alt_u16));
+
+	if(buffer_lengths == NULL)
+	{
+		printf("Bad alloc of 'buffer_lengths' \n");
+		exit(1);
+	}
+
+	if(NUMBER_OF_BUFFERS == 1)
+	{
+		buffer_lengths[0] = image_width*image_height;
+		printf("buffer_lengths[%d] = %u\n",0,buffer_lengths[0]);
+	}
+	else
+	{
+		for(it = 0;it < NUMBER_OF_BUFFERS - 1;it++)
+		{
+			buffer_lengths[it] = MAX_NUM_DMA_TRANSFER;
+			printf("buffer_lengths[%d] = %u\n",it,buffer_lengths[it]);
+		}
+		buffer_lengths[NUMBER_OF_BUFFERS - 1] = image_width*image_height - (NUMBER_OF_BUFFERS - 1)*MAX_NUM_DMA_TRANSFER;
+		printf("buffer_lengths[%d] = %u\n",NUMBER_OF_BUFFERS - 1,buffer_lengths[NUMBER_OF_BUFFERS - 1]);
+	}
+
+	// Allocation of array of input buffers
+	input_buffers = (alt_u8**)malloc(NUMBER_OF_BUFFERS*sizeof(alt_u8*));
+	if(input_buffers == 0)
+	{
+		printf("Bad alloc 'input buffers'\n");
+		exit(1);
+	}
+	// Allocation of buffers
+	for(it = 0;it < NUMBER_OF_BUFFERS;it++)
+	{
+		input_buffers[it] = (alt_u8*)malloc(buffer_lengths[it]*sizeof(alt_u8));
+		if(input_buffers[it] == 0)
+		{
+			printf("Bad alloc 'input buffers[%d]'\n",it);
+			exit(1);
+
+		}
+	}
+
+
+	//image = (alt_u8*)malloc(image_width*image_height*sizeof(alt_u8));
 	histogram = (alt_u32*)malloc((MAX_HISTOGRAM_VALUE + 1)*sizeof(alt_u32));
 	iterator = 0;
 
-	while(iterator < image_width*image_height)
+	//while(iterator < image_width*image_height)
+	//{
+	for(it = 0;it < NUMBER_OF_BUFFERS;it++)
 	{
-		fread(character,1,1,input_file);
-		image[iterator] = character[0];
-		iterator++;
+		for(itr = 0;itr < buffer_lengths[it];itr++)
+		{
+			fread(character,1,1,input_file);
+			input_buffers[it][itr] = character[0];
+			image[iterator] = character[0];
+			iterator++;
+			//iterator++;
+		}
 	}
+	//}
+
+	PERF_START_MEASURING(PERFORMANCE_COUNTER_BASE);
+	PERF_BEGIN(PERFORMANCE_COUNTER_BASE,1);
+	// SOFTWARE
+	hist(image,image_width, image_height, 0, 0, image_width-1, image_height-1, hist_value);
+
+
+	PERF_END(PERFORMANCE_COUNTER_BASE,1);
+
+	free(image);
+	free(hist_value);
+
+	PERF_BEGIN(PERFORMANCE_COUNTER_BASE,2);
+
+	// DMA PROCESSING
+	if(sgdma_m2s == NULL)
+	{
+		printf("Could not open the transmit SG-DMA\n");
+		return 1;
+	}
+
+	if(sgdma_s2m == NULL)
+	{
+		printf("Could not open the receive SG-DMA\n");
+		return 1;
+	}
+
+	// ALOCATION OF TRANCIVE DESCRIPTORS --> M2S
+	temp_ptr = malloc((NUMBER_OF_BUFFERS + 2) * ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE);
+
+	if(temp_ptr == NULL)
+	{
+		printf("Failed to allocate memory for the transmit descriptors\n");
+	    return 1;
+	}
+
+	m2s_desc_copy = (alt_sgdma_descriptor*)temp_ptr;
+
+	while((((alt_u32)temp_ptr) % ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE) != 0)
+	{
+		temp_ptr++;
+	}
+
+	transmit_descriptors = (alt_sgdma_descriptor *)temp_ptr;
+
+
+	transmit_descriptors[NUMBER_OF_BUFFERS].control = 0;
+
+	// ALLOCATION OF RECEIVE DESCRIPTORS
+	temp_ptr = malloc((1 + 2) * ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE);
+
+	if(temp_ptr == NULL)
+	{
+		printf("Failed to allocate memory for the transmit descriptors\n");
+	    return 1;
+	}
+
+	s2m_desc_copy = (alt_sgdma_descriptor*)temp_ptr;
+
+	while((((alt_u32)temp_ptr) % ALTERA_AVALON_SGDMA_DESCRIPTOR_SIZE) != 0)
+	{
+		temp_ptr++;
+	}
+
+	receive_descriptors = (alt_sgdma_descriptor *)temp_ptr;
+
+
+	receive_descriptors[NUMBER_OF_BUFFERS].control = 0;
 
 	// PROCESSING
 
@@ -199,25 +296,23 @@ int main()
 	{
 		alt_avalon_sgdma_construct_mem_to_stream_desc(&transmit_descriptors[buffer_counter],
 													  &transmit_descriptors[buffer_counter+1],
-													  (alt_u32*) image,
-													  (alt_u16) image_width*image_height,
+													  (alt_u32*) input_buffers[buffer_counter],
+													  (alt_u16) buffer_lengths[buffer_counter],
 													  0,
 													  0,
 													  0,
 													  0
 		);
-
-		alt_avalon_sgdma_construct_stream_to_mem_desc(&receive_descriptors[buffer_counter],
-													  &receive_descriptors[buffer_counter + 1],
-													  (alt_u32*) histogram,
-													  (alt_u16) (MAX_HISTOGRAM_VALUE + 1)*sizeof(alt_u32),
-													  0
-		);
-
 	}
-	//(MAX_HISTOGRAM_VALUE + 1)
 
-    //printf("Made descriptors\n");
+	alt_avalon_sgdma_construct_stream_to_mem_desc(&receive_descriptors[0],
+												  &receive_descriptors[1],
+												  (alt_u32*) histogram,
+												  (alt_u16) (MAX_HISTOGRAM_VALUE + 1)*sizeof(alt_u32),
+												  0
+	);
+
+
 
 	alt_avalon_sgdma_register_callback(sgdma_m2s,
 									   &transmit_callback_function,
@@ -234,14 +329,13 @@ int main()
 
 	alt_dcache_flush_all();
 
-	IOWR_32DIRECT(HISTOGRAM_CALCULATION_BASE, 0, 4096);
-	//IOWR_8DIRECT(HISTOGRAM_CALC_BASE, 1, (alt_8)(5));
+	IOWR_32DIRECT(HISTOGRAM_CALCULATION_BASE, 0, image_width*image_height);
 
-	printf("Configure acc done\n");
+	//printf("Configure acc done\n");
 
 	IOWR_32DIRECT(HISTOGRAM_CALCULATION_BASE, 8, 1);
 
-	printf("Started acc done\n");
+	//printf("Started acc done\n");
 
 	/* Start non blocking transfer with DMA modules. */
 	if(alt_avalon_sgdma_do_async_transfer(sgdma_m2s, &transmit_descriptors[0]) != 0)
@@ -253,83 +347,37 @@ int main()
 		printf("Writing the head of the receive descriptor list to the DMA failed\n");
 	}
 
-	printf("%d\n",receive_descriptors[0].bytes_to_transfer);
+	//printf("%d\n",receive_descriptors[0].bytes_to_transfer);
 
-	printf("Started DMA\n");
+	//printf("Started DMA\n");
 	while(tx_done < 1);
 	tx_done = 0;
-	printf("Trancieve done\n");
+	//printf("Trancieve done\n");
 
-	//tmp22 = (alt_u32)IORD_32DIRECT(HISTOGRAM_CALC_BASE, 4);
-	//printf("%u\n",tmp22);
+	//printf("%u\n",(alt_u32)IORD_32DIRECT(HISTOGRAM_CALCULATION_BASE, 4));
 
-	// Start sendindg
-//	while(1)
-//	{
-//
-//		tmp22 = (alt_u32)IORD_32DIRECT(HISTOGRAM_CALC_BASE, 4);
-//		//printf("%u\n",tmp22);
-//		if(tmp22 == 3)
-//		{
-//			printf("End calculating\n");
-//			break;
-//		}
-//
-//	}
-
-	printf("%u\n",(alt_u32)IORD_32DIRECT(HISTOGRAM_CALCULATION_BASE, 4));
-
-	//IOWR_32DIRECT(HISTOGRAM_CALC_BASE, 8, 4);
 	tmp22 = 0;
-	while(rx_done < 1)
-	{
-		//printf("%u\n",(alt_u32)IORD_32DIRECT(HISTOGRAM_CALC_BASE, 4));
-//		if((IORD_32DIRECT(HISTOGRAM_CALC_BASE, 4) & 0x0006) && (tmp22 == 0))
-//		{
-//			printf("End calculating\n");
-//			tmp22 = 1;
-//		}
-//		if((IORD_32DIRECT(HISTOGRAM_CALC_BASE, 4) & 0x0008) && (tmp22 == 1))
-//		{
-//			printf("End sending\n");
-//			tmp22 = 2;
-//		}
-		//printf(".08x",sgdma_s2m)
-		//printf("%u\n",receive_descriptors[0].status);
+	while(rx_done < 1);
 
-	}
+	PERF_END(PERFORMANCE_COUNTER_BASE,2);
 
-	printf("Receive done\n");
+	PERF_STOP_MEASURING(PERFORMANCE_COUNTER_BASE);
+
+
+
+	//printf("Receive done\n");
 	rx_done = 0;
 	alt_avalon_sgdma_stop(sgdma_m2s);
 	alt_avalon_sgdma_stop(sgdma_s2m);
 
-	printf("DMA stop\n");
+	//printf("DMA stop\n");
 
-//	iterator = 0;
-//
-//	while(iterator < 4){
-//
-//		character[0] = (alt_u8)(image_width >> iterator*8);
-//		iterator++;
-//		fwrite(character,1,1,output_file);
-//	}
-//
-//	iterator = 0;
-//
-//	while(iterator < 4){
-//
-//		character[0] = (alt_u8)(image_height >> (iterator*8));
-//		iterator++;
-//		fwrite(character,1,1,output_file);
-//
-//	}
+
 
 
 	for(iterator = 0; iterator < MAX_HISTOGRAM_VALUE + 1;iterator++)
 	{
 
-		//character[0] = MAX_HISTOGRAM_VALUE - image[iterator];
 		for(iterator1 = 0; iterator1 < 4; iterator1++)
 		{
 			character[0] = (alt_u8)(histogram[iterator] >> iterator1*8);
@@ -337,24 +385,26 @@ int main()
 		}
 
 	}
-//	printf("%u\n",(alt_u16)image[0]);
-//	printf("%u\n",(alt_u16)image[1]);
-//	printf("%u\n",(alt_u16)image[2]);
-//	printf("%u\n",(alt_u16)image[3]);
-//	printf("%u\n",(alt_u16)image[4]);
-//	printf("%u\n",image_out[0]);
-//	printf("%u\n",image_out[1]);
-//	printf("%u\n",image_out[2]);
-//	printf("%u\n",image_out[3]);
-//	printf("%u\n",image_out[4]);
 
-	free(image);
+
+	//free(image);
 	free(histogram);
 
 	free(m2s_desc_copy);
 	free(s2m_desc_copy);
 
-	//free(temp_ptr);
+	for(it = 0;it < NUMBER_OF_BUFFERS;it++)
+	{
+		free(input_buffers[it]);
+	}
+	free(input_buffers);
+
+	free(buffer_lengths);
+
+	//printf("HARDWARE  = %u\n",tmp_end_time_hw - tmp_start_time_hw);
+	//printf("SOFTWARE  = %u\n",tmp_end_time_sw - tmp_start_time_sw);
+
+	perf_print_formatted_report((void *)PERFORMANCE_COUNTER_BASE, alt_get_cpu_freq(),2,"software","hardware");
 
 	printf("END!\n");
 
